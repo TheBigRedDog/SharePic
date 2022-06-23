489e1d54161c2eeee5cfd6d39f61baf1c8617855
TheBigRedDog <clmmoran@ucdavis.edu>
2022-06-23 15:26:00 -0700

    setup static file serving

--- image_repository/image_repository/settings.py
+++ image_repository/image_repository/settings.py
@@ -126,8 +126,9 @@ USE_TZ = True
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
 
-STATC_ROOT = os.path.join(BASE_DIR, 'static')
-STATIC_URL = "/static/"
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
+STATIC_URL = "static/"
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
